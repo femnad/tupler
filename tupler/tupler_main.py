930c45a222766cc1385dea86977b24801a151c6f
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2016-10-12 22:03:26 -0400

    Colorify Messages

--- tupler/tupler_main.py
+++ tupler/tupler_main.py
@@ -12,18 +12,28 @@ def _get_credentials(file_name):
         return Credentials(**credentials)
 
 
-def _format_message(message):
-    stream_and_topic = "" if isinstance(message.recipient, list) \
-                       else "\n{} > {}\n".format(
-                               message.recipient,
-                               message.subject)
-    return "{}:{}{}\n".format(message.sender, stream_and_topic,
-                              message.content)
+def _display_message(window, message):
+    window.addstr("{}\n".format(message.sender), curses.color_pair(2))
+    if not isinstance(message.recipient, list):  # Not a private message
+        window.addstr(message.recipient, curses.color_pair(3))
+        window.addstr(" > ")
+        window.addstr(message.subject, curses.color_pair(4))
+        window.addstr("\n")
+    window.addstr("{}\n".format(message.content))
+
+
+def _initialize_colors():
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
+    curses.init_pair(3, curses.COLOR_YELLOW, -1)
+    curses.init_pair(4, curses.COLOR_BLUE, -1)
 
 
 if __name__ == "__main__":
     credentials = _get_credentials('~/.tuplerrc')
     stdscr = curses.initscr()
+    _initialize_colors()
     stdscr.clear()
     curses.noecho()
     curses.cbreak()
@@ -37,7 +47,7 @@ if __name__ == "__main__":
                 break
             sleep(1)
         else:
-            stdscr.addstr(_format_message(message))
+            _display_message(stdscr, message)
             stdscr.refresh()
 
     curses.nocbreak()
